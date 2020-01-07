2326050071e53d7e40cc909b2805ab13a7763050
Aungpao <aungpao321@gmail.com>
2020-01-07 15:44:43 +0700

    v1.0

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -8,15 +8,29 @@ int main(){
 	
 	string name[N];
 	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
-	
+	int x=0;
+	while(x<N){
+		cout << "Name of student = ";
+		cin >> name[x];
+		cout << "Age of student = ";
+		cin >> age[x];
+		x++;
+	}
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
-	
+	int i=0;
+	while(i<N){
+		if(key==age[i]){
+			cout<<name[i]<<"\n";
+			i++;
+		}
+		else{
+			i++;
+		}
+	}
 
 	
 	cout << "--------------------------------------\n";
--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 
+string mixText(string a,string b){
+	string x=a;
+	int L1=a.size();
+	string y=b;
+	int L2=b.size();
+	
+	if(L1==L2){
+		int i=0;
+		while(i<L1){
+			cout<<a[i]<<b[i];
+			i++;
+		}
+	}else{
+		cout<<"E";
+	}
+	return "";
+}
 
 
 
